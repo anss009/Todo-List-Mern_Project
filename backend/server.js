8f893708c2963de68a21d8bf00d12228acc95900
Muhammad Anss <ansslucky07@gmail.com>
2025-12-23 17:43:10 +0200

    updated

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,13 @@ app.use('/api/todos', require('./routes/todos'));
 const mongoURI = 'mongodb+srv://ansslucky07_:anss12345@project1.bxrpzwp.mongodb.net/project1?retryWrites=true&w=majority&appName=project1';
 
 mongoose.connect(mongoURI)
-  .then(() => console.log('✅ MongoDB Connected'))
-  .catch(err => console.log('❌ MongoDB Error:', err));
+  .then(() => console.log('MongoDB is Connected'))
+  .catch(err => console.log('MongoDB Error:', err));
 
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
 app.listen(5000, () => {
-  console.log('🚀 Server running on port 5000');
+  console.log(' Server running on port 5000');
 });
